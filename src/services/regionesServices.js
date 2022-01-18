0615796b16b4b749b23bfe7371e2eadd80f20fe8
betofc11 <60046663+betofc11@users.noreply.github.com>
2022-01-17 21:02:56 -0600

    tratar de arreglar cosas

--- src/services/regionesServices.js
+++ src/services/regionesServices.js
@@ -1,27 +1,26 @@
 const axios = require("axios");
 const config = require('../config.json');
 
-module.exports = {
-    getRegions: async () =>{
-        try {
-            const regions = await axios.get(config.URL_API + "regiones");
-            return JSON.stringify(regions.data);
-        }catch (e) {
-            console.log(e.message)
-        }
-    },
-    getRegionById: async (id) =>{
-        try {
-            const region = await axios.get(config.URL_API + `regiones/${id}`);
-            const vecinos = await axios.get(config.URL_API + `vecinos/getByRegion/${id}`);
-            region.data.vecinos = vecinos.data;
-            console.log(region.data)
-            return JSON.stringify(region.data)
-        } catch (e) {
-            console.log(e.message)
-        }
+exports.getRegions = async () => {
+    try {
+        const regions = await axios.get(config.URL_API + "regiones");
+        return JSON.stringify(regions.data);
+    } catch (e) {
+        console.log(e.message)
     }
 }
+exports.getRegionById = async (id) => {
+    try {
+        const region = await axios.get(config.URL_API + `regiones/${id}`);
+        const vecinos = await axios.get(config.URL_API + `vecinos/getByRegion/${id}`);
+        region.data.vecinos = vecinos.data;
+        console.log(region.data)
+        return JSON.stringify(region.data)
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
 
 
 
